c5f14e5d1978633b304328f89e1bad516c5e9982
Gabriel Silva <86989966+dev-gabrieljs@users.noreply.github.com>
2025-05-26 12:02:16 -0300

    Update Message.java

--- kafka-poc-v6-docker/src/main/java/com/br/kafka_poc/model/Message.java
+++ kafka-poc-v6-docker/src/main/java/com/br/kafka_poc/model/Message.java
@@ -3,23 +3,26 @@ package com.br.kafka_poc.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "messages")
+@Document(collection = "messages") // Indica que esta classe representa um documento da coleção "messages" no MongoDB
 public class Message {
 
-    @Id
+    @Id // Indica que o campo 'id' é a chave primária (identificador único) do documento
     private String id;
+
+    // Campo que armazena o conteúdo da mensagem
     private String content;
 
-    // Getters e Setters
-
+    // Construtor que permite criar uma instância da classe informando apenas o conteúdo
     public Message(String content) {
         this.content = content;
     }
 
+    // Getter para obter o conteúdo da mensagem
     public String getContent() {
         return content;
     }
 
+    // Setter para alterar o conteúdo da mensagem
     public void setContent(String content) {
         this.content = content;
     }
