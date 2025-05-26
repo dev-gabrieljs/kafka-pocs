683d10c9e8b3e71905fc5711062532eec02b7269
Gabriel Silva <86989966+dev-gabrieljs@users.noreply.github.com>
2025-05-26 12:02:49 -0300

    Update KafkaProducer.java

--- kafka-poc-v6-docker/src/main/java/com/br/kafka_poc/producer/KafkaProducer.java
+++ kafka-poc-v6-docker/src/main/java/com/br/kafka_poc/producer/KafkaProducer.java
@@ -5,17 +5,18 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-@EnableKafka
+@Service // Define essa classe como um serviço do Spring, permitindo sua injeção como dependência
+@EnableKafka // Habilita o suporte ao Kafka no contexto da aplicação Spring
 public class KafkaProducer {
 
-
-    @Autowired
+    @Autowired // Injeta automaticamente uma instância de KafkaTemplate
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    // Define o nome do tópico Kafka onde as mensagens serão enviadas
     private String kafkaTopic = "test-topic";
 
+    // Método responsável por enviar mensagens para o Kafka
     public void sendMessage(String message) {
-        kafkaTemplate.send(kafkaTopic, message);
+        kafkaTemplate.send(kafkaTopic, message); // Envia a mensagem para o tópico especificado
     }
 }
