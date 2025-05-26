3369310818469bd4020a63d821f07a4819d994d4
Gabriel Silva <86989966+dev-gabrieljs@users.noreply.github.com>
2025-05-26 12:03:20 -0300

    Update MessageRepository.java

--- kafka-poc-v6-docker/src/main/java/com/br/kafka_poc/repository/MessageRepository.java
+++ kafka-poc-v6-docker/src/main/java/com/br/kafka_poc/repository/MessageRepository.java
@@ -3,6 +3,19 @@ package com.br.kafka_poc.repository;
 import com.br.kafka_poc.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+// Interface que estende MongoRepository, fornecendo operações básicas de acesso a dados no MongoDB
 public interface MessageRepository extends MongoRepository<Message, String> {
-    // Métodos personalizados, se necessário
+    // Ao estender MongoRepository, essa interface herda métodos como:
+    // - save()
+    // - findById()
+    // - findAll()
+    // - deleteById()
+    // - entre outros...
+
+    // <Message, String> indica:
+    // - Message: o tipo da entidade (documento MongoDB)
+    // - String: o tipo do identificador (@Id), que neste caso é uma String
+
+    // Nenhum método adicional é necessário aqui, mas você pode definir consultas personalizadas se precisar
 }
+
